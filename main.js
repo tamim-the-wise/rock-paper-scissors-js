bb862892de91d7824238986118f739383dc70b0b
tamim-the-wise <69663929+tamim-the-wise@users.noreply.github.com>
2024-01-18 12:26:35 +0600

    Add getComputerChoice function to get a randomly generated choice from computer

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+// Declare a FUNCTION getComputerChoice to get computer's choice
+// Declare array choices containing all possible choices
+// Declare index VARIABLE
+// Gnerate a random number from 0 to 1 multiplied by the length of choices array. Round the number and store in index VARIABLE
+// RETURN value of randomly generated index from choices array
+function getComputerChoice() {
+	const choices = ["Rock", "Paper", "Scissors"]
+	let index = Math.floor(Math.random() * choices.length)
+	return choices[index]
+}
